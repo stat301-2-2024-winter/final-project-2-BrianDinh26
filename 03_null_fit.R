aa721f17a9f5bc9dbc94e8bea47c181d35018880
BrianDinh22 <briandinh2022@u.northwestern.edu>
2024-02-20 10:29:50 -0600

    Added null model.

--- 03_null_fit.R
+++ 03_null_fit.R
@@ -1,5 +1,5 @@
 # Final Project, Brian Dinh ----
-# Define and fit ordinary linear regression model.
+# Define and fit null model.
 # We use a random process when fitting, so we have to set a seed beforehand.
 
 # load packages ----
@@ -17,28 +17,28 @@ load(here("results/cars_split.rda"))
 load(here("recipes/recipes.rda"))
 
 set.seed(925)
-# model specifications ----
-lm_spec <- 
-  linear_reg() |> 
-  set_engine("lm") |> 
-  set_mode("regression") 
+# model specifications
+null_spec <- null_model() |> 
+  set_engine("parsnip") |> 
+  set_mode("regression")
 
-# define workflows ----
-lm_wflow <-
-  workflow() |> 
-  add_model(lm_spec) |> 
+# set workflow
+null_workflow <- workflow() |>  
+  add_model(null_spec) |>  
   add_recipe(olr_recipe)
 
-# fit workflows/models ----
 set.seed(925)
-olr_lm_fit <- fit_resamples(lm_wflow, 
-                        resamples = cars_folds)
+null_fit <- null_workflow |> 
+  fit_resamples(
+    resamples = cars_folds, 
+    control = control_resamples(save_workflow = TRUE)
+  )
 
 
 
 # save out results
-save(olr_lm_fit, file = here("results/olr_lm_fit.rda"))
+save(null_fit, file = here("results/null_fit.rda"))
 
 load(here("results/olr_lm_fit.rda"))
 
-olr_lm_fit |> collect_metrics()
+null_fit |> collect_metrics()
