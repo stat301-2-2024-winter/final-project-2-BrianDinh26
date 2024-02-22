76c849e042d484abc817674f8a2bee0450fb9f7e
BrianDinh22 <briandinh2022@u.northwestern.edu>
2024-02-21 22:01:01 -0600

    Fit the knn model and got the weird result of being worse than the ordinary linear regression model.

--- 03_knn_fit.R
+++ 03_knn_fit.R
@@ -22,17 +22,39 @@ tidymodels_prefer()
 load(here("results/cars_split.rda"))
 
 # load pre-processing/feature engineering/recipe
-
+load(here("recipes/sink_recipe.rda"))
 
 set.seed(925)
 # model specifications ----
+knn_spec <- 
+  nearest_neighbor(mode = "regression", 
+                   neighbors = tune()) |> 
+  set_engine("kknn")
 
 
 # define workflows ----
+knn_workflow <- workflow() |> 
+  add_model(knn_spec) |> 
+  add_recipe(sink_recipe)
+
+# check hyperparameters
+knn_params <- hardhat::extract_parameter_set_dials(knn_spec)
+# not set yet
+
+# grid
+elastic_grid <- grid_regular(knn_params, levels = 7)
 
 # fit workflows/models ----
 set.seed(925)
+tuned_knn <- tune_grid(knn_workflow,
+                       cars_folds,
+                       grid = elastic_grid,
+                       control = control_grid(save_workflow = TRUE))
 
+# write out results (fitted/trained workflows) ----
+save(tuned_knn, file = here("results/tuned_knn.rda"))
 
-# save out results
-
+tuned_knn |> 
+  collect_metrics() |>
+  filter(.metric == "rmse") |> 
+  arrange((mean))
