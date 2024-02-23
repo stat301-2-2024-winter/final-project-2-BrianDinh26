3848feca334a07cf5c2bc2822d9e2cb389047ba2
BrianDinh22 <briandinh2022@u.northwestern.edu>
2024-02-23 13:59:47 -0600

    Working on model analysis.

--- 04_model_analysis.R
+++ 04_model_analysis.R
@@ -23,9 +23,14 @@ load(here("results/cars_split.rda"))
 # load fit data
 load(file = here("results/null_fit.rda"))
 load(file = here("results/olr_lm_fit.rda"))
+load(file = here("results/rf_fit.rda"))
+load(file = here("results/tuned_bt.rda"))
+load(file = here("results/tuned_elastic.rda"))
+load(file = here("results/tuned_knn.rda"))
 
 
-# combine initial table for progress memo #2
+
+#combine all models into a table
 table_null <- null_fit |> collect_metrics() |> 
   filter(.metric == 'rmse') |> 
   mutate(model = "null")
@@ -33,6 +38,22 @@ table_null <- null_fit |> collect_metrics() |>
 table_olr <- olr_lm_fit |> collect_metrics() |> 
   filter(.metric == 'rmse') |> 
   mutate(model = "olr")
+
+table_rf <- rf_fit |> collect_metrics() |> 
+  filter(.metric == 'rmse') |> 
+  mutate(model = "rf")
+
+table_bt <- tuned_bt |> collect_metrics() |> 
+  filter(.metric == 'rmse') |> 
+  mutate(model = "bt")
+
+table_elastic <- tuned_elastic |> collect_metrics() |> 
+  filter(.metric == 'rmse') |> 
+  mutate(model = "elastic")
+
+table_elastic <- tuned_knn |> collect_metrics() |> 
+  filter(.metric == 'rmse') |> 
+  mutate(model = "knn")
 
 pm_2_table <- bind_rows(table_null, table_olr) |> 
   select(model, .metric, mean, std_err) |> 
